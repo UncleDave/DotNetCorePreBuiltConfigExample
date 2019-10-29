0b6fda508135e4e0df16e7f046e713f7e4407679
UncleDave <3244206+UncleDave@users.noreply.github.com>
2019-10-29 21:29:17 +0000

    Initial commit

---
+++ PreBuiltConfigExample.Logging.Loud/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace PreBuiltConfigExample.Logging.Loud
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builtConfig = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddCommandLine(args)
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File(builtConfig["Logging:FilePath"])
+                .CreateLogger();
+
+            try
+            {
+                new HostBuilder()
+                    .ConfigureLogging(logging =>
+                    {
+                        logging.AddSerilog();
+                    })
+                    .ConfigureAppConfiguration(config =>
+                    {
+                        // Throw an example exception
+                        // In reality this could be any exception from anywhere within Host.Build()
+                        // Exception will be logged with Serilog
+                        throw new ApplicationException("Oh no, it broke!");
+                        config.AddConfiguration(builtConfig);
+                    })
+                    .Build()
+                    .Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host blew up!");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+    }
+}
---
+++ PreBuiltConfigExample.Logging.Silent/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace PreBuiltConfigExample.Logging.Silent
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new HostBuilder()
+                .ConfigureLogging((context, logging) =>
+                {
+                    logging.AddSerilog(
+                        new LoggerConfiguration()
+                            .WriteTo.Console()
+                            .WriteTo.File(context.Configuration["Logging:FilePath"])
+                            .CreateLogger()
+                    );
+                })
+                .ConfigureAppConfiguration(config =>
+                {
+                    // Throw an example exception
+                    // In reality this could be any exception from anywhere within Host.Build()
+                    // Exception will not be logged with Serilog, only to the Console
+                    throw new ApplicationException("Oh no, it broke!");
+                })
+                .Build()
+                .Run();
+        }
+    }
+}
